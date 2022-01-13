e0b8688109ebf9d72bf8b04782f03dfbafde9e41
itvn9online <itvn9online@gmail.com>
2022-01-13 08:47:33 +0700

    sync

--- address_server.js
+++ address_server.js
@@ -2,11 +2,8 @@
 *
 * Chức năng tạo private key theo thứ tự tuần hoàn sau đó tạo địa chỉ ví từ key đó -> tiếp đến là check xem ví có tiền không?
 *
-* Lệnh chạy trên window
+* Lệnh chạy
 cd ~ ; cd F:\AppServ\www\nodejs\privatekeys ; node run
-*
-* Lệnh chạy trên linux
-/usr/bin/node /root/njs/privatekeys/address_hex.js
 *
 * Tài liệu tham khảo
 https://github.com/cryptocoinjs/coinkey
@@ -16,32 +13,25 @@ https://github.com/cryptocoinjs/coinkey
 //
 'use strict';
 
-// tạo ví BTC
-var CoinKey = require('coinkey');
-// tạo ví ETH
-var ethers = require('ethers');
-//var crypto = require('crypto');
 //
+var CoinKey = require('coinkey');
 var fs = require('fs');
 var request = require('request');
-var http = require('http2');
+//var http = require('http2');
 var http = require('http');
 
 
 /*
  * config
  */
-var debug_code = false;
-var debug_code = true;
+//var debug_code = true;
+//var debug_code = false;
 
 // độ dài của chuỗi khóa của bitcoin dạng HEX
 var str64 = '';
 // số vòng lặp để lặp hết các khóa -> 1 con số vĩ đại đấy
 for (var i = 0; i < 64; i++) {
     str64 += '0';
-}
-if (debug_code === true) {
-    console.log('str64: ', str64);
 }
 
 //
@@ -51,17 +41,28 @@ var userAgent = 'request';
 
 // tạo private key theo thứ tự
 var privKey = '';
+// cứ mỗi vòng lặp key dạng số thì thêm 1 key dạng chữ vào
+//var forAlphaKey = 0;
+//var publicKey = '';
 // trả về tối đa mỗi lần 100 địa chỉ ví để check thôi
 var max_while = 7;
-if (debug_code === true) {
-    max_while = 50;
-}
+var show_i = 0;
+// vị trí để add dữ liệu vào mảng -> tối đa scan được 100 địa chỉ thôi, nên phải chia làm 2 đợt
+var add_i = 0;
 var result_data = {};
-var temp_data = []; // BTC address
-var eth_add = []; // ETH address
+var temp_data = [
+    []
+];
 
 // xác định trang scan lần trước -> các địa chỉ ví tiếp theo sẽ scan
 var page_path = __dirname + '/page.txt';
+var old_page_scan = 0;
+if (fs.existsSync(page_path)) {
+    old_page_scan = fs.readFileSync(page_path).toString();
+    old_page_scan *= 1;
+    console.log('Page scan (log): ', old_page_scan);
+}
+//var old_page_scan = 0;
 
 
 /*
@@ -81,24 +82,29 @@ function create_address(str) {
     var ck = new CoinKey(new Buffer.from(privKey, 'hex'))
 
     // trả về danh sách địa chỉ ví để máy trạm tự check
+    //console.log(show_i, add_i, privKey, ck.publicAddress);
     //console.log(privKey);
     //console.log(privKey, ck.publicAddress);
     //console.log(str);
 
     //
+    //temp_data[add_i].push(privKey + ' ' + ck.publicAddress);
     // chỉ cần hiển thị phần chuỗi chính, các số 0 sẽ bổ sung sau -> nhẹ file -> tải nhanh
-    //temp_data.push(ck.publicAddress);
-    temp_data.push(str + ' ' + ck.publicAddress);
-
-    /*
-     * ETH
-     */
-    var eth_wallet = new ethers.Wallet('0x' + privKey);
+    temp_data[add_i].push(str + ' ' + ck.publicAddress);
+    //temp_data[add_i].push(str);
+    //temp_data[add_i].push(privKey);
 
     //
-    //eth_add.push(eth_wallet.address);
-    eth_add.push(str + ' ' + eth_wallet.address);
-    //eth_add.push(privKey + ' ' + eth_wallet.address);
+    show_i++;
+
+    // chia ra để tránh quá 100 ví / mảng
+    if (show_i % 90 == 0) {
+        // thêm mảng mới
+        temp_data.push([]);
+
+        // vòng lặp tới sẽ add vào mảng mới
+        add_i++;
+    }
 }
 
 // key kèm chữ
@@ -124,74 +130,90 @@ function key_dang_chu(str) {
 /*
  * RUN
  */
+var begin_i = old_page_scan;
 
 function ___run() {
-    //console.log('- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -');
-
-    //
-    var begin_i = 0;
-    if (fs.existsSync(page_path)) {
-        begin_i = fs.readFileSync(page_path).toString();
-        begin_i *= 1;
-        //console.log('Page scan (log): ', begin_i);
-    }
+    var begin_scan = Date.now();
+    var str = '';
     var max_for = begin_i + max_while;
-
     // bắt đầu scan từ ví số 1
     if (begin_i <= 0) {
         begin_i = 1;
+        /*
+    } else {
+        forAlphaKey = begin_i / 10;
+        */
     }
-    console.log('begin i: ', begin_i, 'max for: ', max_for);
-    //console.log('function caller: ' + arguments.callee.caller.name.toString());
+    //console.log('begin i: ', begin_i);
+    //console.log('max for: ', max_for);
+    //console.log('forAlphaKey: ', forAlphaKey);
     //return false;
 
     //
-    temp_data = [];
-    //var eth_key = []; // ETH private key
-    eth_add = [];
+    temp_data = [
+        []
+    ];
+    show_i = 0;
+    add_i = 0;
 
     //
-    var begin_scan = Date.now();
-    var str = '';
     for (var i = begin_i; i < max_for; i++) {
         str = i.toString();
 
         //
         create_address(str);
-        //key_dang_chu(str);
+        key_dang_chu(str);
+
+        // cứ hết 1 vòng số thì tăng 1 vòng chữ lên -> check key bao gồm cả chữ
+        /*
+        if (i % 10 == 9) {
+            if (forAlphaKey > 0) {
+                key_dang_chu(forAlphaKey.toString());
+            }
+
+            //
+            forAlphaKey++;
+        }
+        */
     }
+
+    //
     var end_scan = Date.now();
     end_scan = (end_scan - begin_scan) / 1000;
     //console.log('Created time: ', end_scan);
 
     //
     result_data = {
-        //'ek': eth_key, // ETH private key
-        'e': eth_add, // ETH address
-        'b': temp_data,
-        'len': temp_data.length,
+        'data': temp_data,
+        'len': show_i - 1,
         'time': end_scan,
-        // chỉ in ra chữ begin thôi -> sau dò được thì sẽ vào tìm lại private dựa theo begin
-        'begin': begin_i,
-        'while': max_while - 1,
-        'next': max_for,
         'status': 1
     };
     //console.log('Result data: ', result_data);
 
+    // chuẩn bị cho lượt mới
+    //old_page_scan++;
+    begin_i = max_for;
+
     //
     fs.writeFile(page_path, max_for.toString(), function (err) {
         if (err) throw err;
-        //console.log('Saved (write)! ' + page_path);
+        console.log('Saved (write)! ' + page_path);
     });
 
+    // TEST
+    /*
+    setTimeout(function () {
+        ___run();
+    }, 2000);
+    */
+    
     //
     return result_data;
 }
 //
-if (debug_code === true) {
-    console.log('Result data: ', ___run());
-}
+//___run();
+
 
 /*
  * tạo kết nối qua https
@@ -249,9 +271,7 @@ if (requestIP != '') {
             console.log('Request getipaddress status:', res.statusCode);
         } else {
             // TEST
-            if (debug_code === true) {
-                data.ip = '127.0.0.1';
-            }
+            data.ip = '127.0.0.1';
 
             //
             console.log('Open host:');
@@ -279,10 +299,7 @@ if (requestIP != '') {
                 });
 
                 //
-                var result_data = ___run();
-
-                //
-                response.end(JSON.stringify(result_data));
+                response.end(JSON.stringify(___run()));
             }).listen(open_port, data.ip);
         }
     });
